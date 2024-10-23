60a5f82ead40408ebd0577e42e9b23975f12bc97
Robin Krahl <robin@nitrokey.com>
2024-10-23 23:53:07 +0200

    Update python-nitrokey to v0.2.2

--- pkgs/python-nitrokey/default.nix
+++ pkgs/python-nitrokey/default.nix
@@ -1,12 +1,12 @@
 { fetchPypi, python3Packages }:
 python3Packages.buildPythonPackage rec {
   pname = "nitrokey";
-  version = "0.2.0";
+  version = "0.2.2";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-oWfhy7Mj1UnrC70G7fpkl4q4uG0CdvO0BHwfJYszR30=";
+    hash = "sha256-tG6+diyrauJEzpPG33+S5o1ik3n44/443szR7vXH4gE=";
   };
 
   nativeBuildInputs = [ python3Packages.pythonRelaxDepsHook ];
